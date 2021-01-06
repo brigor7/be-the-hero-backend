5d3d18dbbd13fb8c0e374651e693957611fe968b
=Bruno Igor <=brigor7@gmail.com>
2021-01-06 16:08:53 -0300

    Incluindo cors no backend

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
-
+//app.use(cors({  origin:'http://endereco.com' }));//Somente aplicação desse endereço acessa backend
+app.use(cors()); //Todas aplicações acessam backend
 app.use(routes);
-
 app.listen(3333);
